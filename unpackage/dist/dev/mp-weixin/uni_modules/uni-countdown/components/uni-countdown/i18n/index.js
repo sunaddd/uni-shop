e8d56d98e3cdf7833deffb55d1577f44a7b52251
sunaddd <2238369245@qq.com>
2023-01-24 23:59:37 +0800

    init project

---
+++ unpackage/dist/dev/mp-weixin/uni_modules/uni-countdown/components/uni-countdown/i18n/index.js
@@ -0,0 +1,25 @@
+"use strict";
+const en = {
+  "uni-countdown.day": "day",
+  "uni-countdown.h": "h",
+  "uni-countdown.m": "m",
+  "uni-countdown.s": "s"
+};
+const zhHans = {
+  "uni-countdown.day": "\u5929",
+  "uni-countdown.h": "\u65F6",
+  "uni-countdown.m": "\u5206",
+  "uni-countdown.s": "\u79D2"
+};
+const zhHant = {
+  "uni-countdown.day": "\u5929",
+  "uni-countdown.h": "\u6642",
+  "uni-countdown.m": "\u5206",
+  "uni-countdown.s": "\u79D2"
+};
+const messages = {
+  en,
+  "zh-Hans": zhHans,
+  "zh-Hant": zhHant
+};
+exports.messages = messages;
---
+++ unpackage/dist/dev/mp-weixin/uni_modules/uni-fav/components/uni-fav/i18n/index.js
@@ -0,0 +1,19 @@
+"use strict";
+const en = {
+  "uni-fav.collect": "collect",
+  "uni-fav.collected": "collected"
+};
+const zhHans = {
+  "uni-fav.collect": "\u6536\u85CF",
+  "uni-fav.collected": "\u5DF2\u6536\u85CF"
+};
+const zhHant = {
+  "uni-fav.collect": "\u6536\u85CF",
+  "uni-fav.collected": "\u5DF2\u6536\u85CF"
+};
+const messages = {
+  en,
+  "zh-Hans": zhHans,
+  "zh-Hant": zhHant
+};
+exports.messages = messages;
